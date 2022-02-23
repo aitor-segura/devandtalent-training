1a1a71bed092fb961a38ce6a265ba8809b5a1278
Aitor Segura <sb_aitor@hotmail.com>
2022-02-23 12:38:08 +0100

    init server

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({"message": "Hola Mundo!"});
+});
+
+app.listen(3000, ()=> {
+    console.log("Server is listening on port 80");
+});
