ce5d841431d539707e4e9753cc3f008e3a1712cd
Aitor Segura <sb_aitor@hotmail.com>
2022-02-25 09:13:45 +0100

    Implementing Rollbar

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import Rollbar from 'rollbar';
+
+const rollbar = new Rollbar({
+  accessToken: '98f8082144844f56a6d2e8448f982e90',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hola Mundo!' });
+  rollbar.log('Hello world');
 });
 
 app.listen(80, () => {
-  console.log('Server is listening on port 80');
+  rollbar.log('Server is listening on port 80');
 });
