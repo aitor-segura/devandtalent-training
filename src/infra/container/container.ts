fc1c7e84b32404af7383e11182372cc8fd03b397
Aitor Segura <sb_aitor@hotmail.com>
2022-03-07 10:40:44 +0100

    Update container

--- src/infra/container/container.ts
+++ src/infra/container/container.ts
@@ -1,19 +1,42 @@
-import type { AppContainer } from '.';
-import { asFunction, asValue, createContainer } from 'awilix';
+import path from 'path';
+import type { AppCradle } from '.';
+import { asFunction, asValue, createContainer, Lifetime } from 'awilix';
 import { scopePerRequest } from 'awilix-express';
-import envs, { AppConfig } from '@config/env';
+import config, { AppConfig, AppEnvs } from '@config/env';
 import logger, { Logger } from '@infra/logger';
 import tracker, { Tracker } from '@infra/tracker';
-import server, { AppMiddleware, AppServer } from '@interfaces/AppServer';
+import validator, { Validator } from '@infra/validator';
+import server, { AppMiddleware, AppServer } from '@interfaces/app/server';
+import errorHttpHandler from '@interfaces/app/server/middlewares/errorHandler';
+import { ErrorHttpHandler } from '@interfaces/app/server/middlewares/types';
 
-const container = createContainer<AppContainer>();
+const { env } = config;
+
+const container = createContainer<AppCradle>();
 
 container.register({
-  config: asValue<AppConfig>(envs),
+  config: asValue<AppConfig>(config),
   logger: asFunction<Logger>(logger).singleton(),
   tracker: asFunction<Tracker>(tracker).singleton(),
+  validator: asFunction<Validator>(validator).singleton(),
   server: asFunction<AppServer>(server).singleton(),
+  errorHttpHandler: asFunction<ErrorHttpHandler>(errorHttpHandler).singleton(),
 });
+
+container.loadModules(
+  [
+    `application/**/*.app.{js,ts}`,
+    `infra/persistence/${env[AppEnvs.PERSISTENCE]}/repositories/**/*.repository.{js,ts}`,
+  ],
+  {
+    formatName: 'camelCase',
+    resolverOptions: {
+      register: asFunction,
+      lifetime: Lifetime.SCOPED,
+    },
+    cwd: path.resolve('src'),
+  },
+);
 
 container.register({
   containerMiddleware: asValue<AppMiddleware>(scopePerRequest(container)),
--- src/infra/container/types.ts
+++ src/infra/container/types.ts
@@ -1,14 +1,24 @@
 import type { AppConfig } from '@config/env';
 import type { Logger } from '@infra/logger';
 import type { Tracker } from '@infra/tracker';
-import type { AppServer, AppMiddleware } from '@interfaces/AppServer';
+import type { Validator } from '@infra/validator';
+import type { AppServer, AppMiddleware } from '@interfaces/app/server';
+import type { UserCradleApp } from '@application/User';
+import type { UserCradleRepository } from '@infra/persistence/inMemory/User/types';
+import { AwilixContainer } from 'awilix';
+import { ErrorHttpHandler } from '@interfaces/app/server/middlewares/types';
 
-export type AppContainer = {
+export type AppCradle = {
   config: AppConfig;
   logger: Logger;
   tracker: Tracker;
+  validator: Validator;
   server: AppServer;
+  errorHttpHandler: ErrorHttpHandler;
   containerMiddleware: AppMiddleware;
-};
+} & UserCradleApp &
+  UserCradleRepository;
 
+export type AppContainer = AwilixContainer<AppCradle>;
 
+export type DependencyCradled<T> = (container: AppCradle) => T;
